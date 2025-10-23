d27ea1bd271b7676647b68dc0f154227284b0459
Tonmoy <tonmoybk198@gmail.com>
2025-10-23 20:07:14 +0530

    Cleaning todoList using reducer function and Use of Dispatcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import "./App.css";
 import AddTodo from "./components/AddTodo/AddTOdo";
 import TodoList from "./components/TodoList/TodoList";
 import TodoContext from "./context/TodoContext";
+import TodoDispatchContext from "./context/TodoDispatchContext";
+import todoReducer from "./reducers/todoReducer";
 function App() {
-  const [list, setList] = useState([
-    { id: 1, todoData: "todo 1", finished: false },
-    { id: 2, todoData: "todo 2", finished: false },
-  ]);
+  // const [list, setList] = useState([
+  //   { id: 1, todoData: "todo 1", finished: false },
+  //   { id: 2, todoData: "todo 2", finished: false },
+  // ]);
+
+  const [list, dispatch] = useReducer(todoReducer, []);
 
   return (
-    <TodoContext.Provider value={{ list, setList }}>
-      <AddTodo
-        updateList={(todo) =>
-          setList([
-            ...list,
-            { id: list.length + 1, todoData: todo, finished: false },
-          ])
-        }
-      />
-      <TodoList />
+    <TodoContext.Provider value={{ list }}>
+      <TodoDispatchContext.Provider value={{ dispatch }}>
+        <AddTodo />
+        <TodoList />
+      </TodoDispatchContext.Provider>
     </TodoContext.Provider>
   );
 }
